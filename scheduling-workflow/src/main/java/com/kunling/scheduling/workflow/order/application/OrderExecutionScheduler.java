21cab346d95505dd30450baea7750c9b2bdf7081
tjh <tangjihua@sundear.com>
2026-08-25 14:52:06 +0800

    流程第7版  加入订单模块

---
+++ scheduling-workflow/src/main/java/com/kunling/scheduling/workflow/order/application/OrderExecutionScheduler.java
@@ -0,0 +1,24 @@
+package com.kunling.scheduling.workflow.order.application;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrderExecutionScheduler {
+    private final OrderTaskOrchestrationService orchestrationService;
+
+    public OrderExecutionScheduler(OrderTaskOrchestrationService orchestrationService) {
+        this.orchestrationService = orchestrationService;
+    }
+
+//    @Scheduled(fixedDelayString = "${kunling.workflow.order-execution.fixed-delay-ms:5000}")
+    public void dispatch() {
+        try {
+            orchestrationService.dispatchNext();
+        } catch (RuntimeException exception) {
+            log.error("订单任务定时调度失败", exception);
+        }
+    }
+}
